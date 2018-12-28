76bdd9cd1aaa763ce372bf91b1d19c2ece430a1c
Dmitry Makhnev <dmitry.ivshin@gmail.com>
2018-12-28 11:27:13 +0300

    some js from workshop

---
+++ src/js/UI/components/TodoListComponent.js
@@ -0,0 +1,24 @@
+import { createFromTemplate } from '../../utils/templatesManager';
+
+
+export class TodoListComponent {
+
+    constructor(root) {
+        this._root = root;
+    }
+
+    addTodo(todoText) {
+        const newItemHTML = createFromTemplate(
+            'todoItemTemplate',
+            {
+                text: todoText,
+            }
+        );
+        console.log(newItemHTML);
+
+        this._root.appendChild(newItemHTML);
+    }
+
+
+}
+
